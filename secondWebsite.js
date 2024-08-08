a30a32b6627d8e540b134644f0c7521bcbf8113b
Johan Aurel Botouli <aurel.botouli@gmail.com>
2024-08-08 17:55:42 +0100

    created secondWebsite react component and implementing it

---
+++ secondWebsite.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+function SecondWebsite() {
+    return (
+        <div>
+            <header>
+                <h1 id="header" draggable='true'>Johan's Document</h1>
+            </header>
+            <p>
+                First, my love for Morse code. But how can I pretend to love Morse code when I don't use it, you might think...
+                Interesting . -·-· 
+                --··-- ·
+                ·- -- - -·- -·-- --·· ·---- ·- ·-- --·- ···-- ----· --- -·· ···· -·-- · -·-· ···- ·· ·-- 
+                ··- ····- -···· --··· ·· · ···-- --
+                --· ···
+
+                · --· ·-·· ··--- ··--·· ·---- ···-- -···· --·
+            </p>
+            <div>
+                <ul>
+                    <li>·- -··</li> 
+                    <li>··-· -·</li>
+                    <li>--- --</li>
+                    <li>--· -··</li>
+                    <li>·---- --</li>
+                </ul>  
+                <ol>
+                    <li>-- ···-</li>
+                    <li>-···· </li>
+                    <li>··--··</li>
+                    <li>··-- --</li>
+                </ol>
+            </div>
+            <hr />
+            <a href="https://www.bbcgoodfood.com/recipes/ultimate-easy-gingerbread">
+                <img src="./86151051.jpg" alt="a gingerbread made Groot sitting next to a woman that probably made it" />
+            </a>
+            <br />
+            <hr />
+            <p>
+                ---- ··· --·- ··--- · ·--· ····· ·-·-·- ··· - -·-· ·-·· -· ·-· ··· ··-· ·-·· ··--·· · --- -··· -----
+                ·-· -·· -··- ··--·· ·- - -··- ----- ····· --· -·· ·--· ---·· ---
+                ··- · --- ····- ---·· · --
+                · ·-·· ·---- ---·· - ··- -· --·- ····- · ·--· ----- ··· -- -·· --··· ----· ·-· ·-- -·-· ·---- ·
+            </p>
+            <a  id="top"href="#header"><h5>BACK TO TOP</h5></a>
+            <a  id="johan"href="https://en.wikipedia.org/wiki/Johan_(given_name)" >Here is more
+             about Johan(the name)</a><p>and</p>
+        <h id="morse" href="https://www.history.navy.mil/content/dam/museums/undersea/education/Morse%20Code.pdf" target="_blank">More about morse </h>
+    <footer><h5><b>Done with my Document</b></h5></footer>
+        </div>
+    );
+}
+
+export default SecondWebsite;
